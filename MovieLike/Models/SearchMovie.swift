f62db81a95b92f0df38d03eb0fd6aee8848bd242
jannchoi <gongjo3695@gmail.com>
2025-01-27 23:28:50 +0900

    searhView

---
+++ MovieLike/Models/SearchMovie.swift
@@ -0,0 +1,8 @@
+//
+//  SearchMovie.swift
+//  MovieLike
+//
+//  Created by 최정안 on 1/27/25.
+//
+
+import Foundation
---
+++ MovieLike/Models/TrendMovie.swift
@@ -0,0 +1,8 @@
+//
+//  TrendMovie.swift
+//  MovieLike
+//
+//  Created by 최정안 on 1/27/25.
+//
+
+import Foundation
